cf7d776ed0e0c073fdcba6cb0089fb7027fca526
Gurpreet <lostlegend2701@gmail.com>
2021-07-25 22:57:01 +0530

    adding cors again

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
-// const cors = require('cors');
+const cors = require('cors');
 const knex = require('knex');
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
@@ -23,7 +23,7 @@ const db = knex({
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
-// app.use(cors());
+app.use(cors());
 
 app.get('/', (req,res) => res.json('Server is working.'))
 app.post('/signin', (req, res) => {signin.handleSignIn(req, res, db, bcrypt)})
