ee973bbde413b08aa2373aebfa755181669b6c99
Gurpreet <lostlegend2701@gmail.com>
2021-07-30 23:44:55 +0530

    JWT update

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,25 +1,33 @@
 const handleSignIn = (req, res, db, bcrypt) => {
+    //AUTHENTICATION
     const {email, password} = req.body;
     if(!email || !password){
         return res.status(400).json('Incorrect form submission');
     }
     db.select('email', 'hash').from('login')
-        .where({email: email})
-        .then(async (userInfo) => {
-            const isValid = await bcrypt.compare(password, userInfo[0].hash);
-            if(isValid){
+    .where({email: email})
+    .then(async (userInfo) => {
+        const reqUser = userInfo[0];
+        const isValid = await bcrypt.compare(password, reqUser.hash);
+        if(isValid){
+            // JWT AUTHORIZATION
+            const reqUser = {email: email, password: password};
+            accessToken = jwt.sign(reqUser, process.env.ACCESS_TOKEN_SECRET, (err, tokenID) => {
                 return db.select('*').from('users')
                 .where({email: email})
                 .then(user => {
                     res.status(200).json(user[0])
                 })
                 .catch(err => res.status(400).json('Unable to get user !'))
-            }
-            else {
-                res.status(400).json('Wrong credentials');
-            }
-        })
-        .catch(err => res.status(400).json('Wrong credentials'));
+            });
+        }
+        else {
+            res.status(400).json('Wrong credentials');
+        }
+    })
+    .catch(err => res.status(400).json('Wrong credentials'));
+
+   
 }
 
 module.exports = {
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const knex = require('knex');
 const register = require('./controllers/register');
