a303a1fe6812f55cddfa8869da163c13eba37a85
Thanusha Mandala <MandalaThanusha_NagaVenkat@intuit.com>
2022-10-22 12:02:00 +0530

    feat: added eslint to the project

--- server.js
+++ server.js
@@ -1,39 +1,37 @@
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt');
-const cors = require('cors');
-const knex = require('knex');
-const register = require('./controllers/register');
-const signin = require('./controllers/signin');
-const profile = require('./controllers/profile');
-const image = require('./controllers/image');
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const bcrypt = require('bcrypt')
+const cors = require('cors')
+const knex = require('knex')
+const register = require('./controllers/register')
+const signin = require('./controllers/signin')
+const profile = require('./controllers/profile')
+const image = require('./controllers/image')
 
-//knex initilization of database
+// knex initilization of database
 const db = knex({
-    client: 'pg',
-    connection: {
-      connectionString : process.env.DATABASE_URL,
-      ssl: {
-        rejectUnauthorized: false
-      }
+  client: 'pg',
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
     }
-});
+  }
+})
 
-const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(cors());
+const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors())
 
-app.get('/', (req,res) => res.json('Server is working.'))
-app.post('/signin', (req, res) => {signin.handleSignIn(req, res, db, bcrypt)})
-app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
-app.get('/profile/:id', (req,res) => {profile.handleProfileGet(req, res, db)})
-app.put('/image', (req,res) => {image.handleImage(req, res, db)})
-app.post('/imageurl', (req,res) => {image.handleApiCall(req, res)})
-
-
+app.get('/', (req, res) => res.json('Server is working.'))
+app.post('/signin', (req, res) => { signin.handleSignIn(req, res, db, bcrypt) })
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
+app.put('/image', (req, res) => { image.handleImage(req, res, db) })
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log(`Server is running at http://localhost:${process.env.PORT}`);
+  console.log(`Server is running at http://localhost:${process.env.PORT}`)
 })
